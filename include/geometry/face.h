5abfa3c51f45eb0ebdeda7c36de02f16de314fc6
chalettt <wrrw2006@gmail.com>
2026-03-03 15:22:57 +0100

    feat(main): doxygen doc for faces and models.

--- include/geometry/face.h
+++ include/geometry/face.h
@@ -3,14 +3,36 @@
 
 #include "geometry/point.h"
 
+/**
+ * @brief Structure to hold data of faces.
+ */
 typedef struct face
 {
     Point **points;
     Point *normal;
 } Face;
 
+/**
+ * @brief Creates a face.
+ *
+ * @param points The array of points which defines a face.
+ * @return The created face.
+ */
 Face *create_face(Point **points);
+
+/**
+ * @brief Destroys (frees) a face.
+ *
+ * @param face The face to destroy.
+ */
 void destroy_face(Face *face);
+
+/**
+ * @brief Calculates the normal of a face.
+ *
+ * @param face The face to calculate the normal of.
+ * @return The face normal.
+ */
 Point *get_face_normal(Face *face);
 
 #endif /* TRIANGE_H */
--- include/geometry/model.h
+++ include/geometry/model.h
@@ -3,17 +3,47 @@
 
 #include "geometry/face.h"
 
+/**
+ * @brief Default face count of models.
+ */
 #define FACE_COUNT 32768
+
+/**
+ * @brief Default vertex count of models.
+ */
 #define VERTEX_COUNT 32768
 
+/**
+ * @brief Structure to hold data relative to the loaded model.
+ */
 typedef struct model
 {
     Face **faces;
     Point *origin;
 } Model;
 
+/**
+ * @brief Loads a model.
+ *
+ * @param path The path to the wavefront file.
+ * @param origin The origin of the model.
+ * @return The loaded model.
+ */
 Model *load_model(char *path, Point *origin);
+
+/**
+ * @brief Destroys (frees) a model.
+ *
+ * @param model The model to destroy.
+ */
 void destroy_model(Model *model);
+
+/**
+ * @brief Rotates a model.
+ *
+ * @param model The model to rotate.
+ * @param alpha The angle to rotate the model by.
+ */
 void rotate_model(Model *model, double alpha);
 
 #endif /* MODEL_H*/
