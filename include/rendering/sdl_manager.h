b3498656a0ae9471538d16df02868d19ed77cb76
chalettt <wrrw2006@gmail.com>
2026-03-03 15:24:09 +0100

    refactor(main): Removed sdl_image imports since it is not used.

--- include/rendering/sdl_manager.h
+++ include/rendering/sdl_manager.h
@@ -2,7 +2,6 @@
 #define SDL_MANAGER_H
 
 #include <SDL2/SDL.h>
-#include <SDL2/SDL_image.h>
 
 /**
  * @brief Window width.
--- src/rendering/sdl_manager.c
+++ src/rendering/sdl_manager.c
@@ -1,7 +1,6 @@
 #include "rendering/sdl_manager.h"
 
 #include <SDL2/SDL.h>
-#include <SDL2/SDL_image.h>
 
 #include "utils/debug.h"
 
